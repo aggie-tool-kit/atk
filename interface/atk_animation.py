cbd5fe366d932aa34c5fc9fac30b3e587542c3ac
Jeff Hykin <jeff.hykin@gmail.com>
2019-06-08 00:48:01 -0500

    _

--- interface/atk_animation.py
+++ interface/atk_animation.py
@@ -1,4 +1,3 @@
-
 from pyfiglet import Figlet
 
 from asciimatics.effects import Scroll, Mirage, Wipe, Cycle, Matrix, BannerText, Stars, Print
@@ -13,14 +12,14 @@ import sys
 def _credits(screen):
     scenes = []
 
-    text = Figlet(font="banner", width=200).renderText("ASCIIMATICS")
+    text = Figlet(font="banner", width=200).renderText("ATK")
     width = max([len(x) for x in text.split("\n")])
 
     effects = [
         Matrix(screen, stop_frame=200),
         Mirage(
             screen,
-            FigletText("Asciimatics"),
+            FigletText("ATK"),
             screen.height // 2 - 3,
             Screen.COLOUR_GREEN,
             start_frame=100,
@@ -28,13 +27,13 @@ def _credits(screen):
         Wipe(screen, start_frame=150),
         Cycle(
             screen,
-            FigletText("Asciimatics"),
+            FigletText("ATK"),
             screen.height // 2 - 3,
             start_frame=200)
     ]
     scenes.append(Scene(effects, 250, clear=False))
 
-    screen.play(scenes, stop_on_resize=True)
+    screen.play(scenes, stop_on_resize=True, repeat=False)
 
 
 if __name__ == "__main__":
