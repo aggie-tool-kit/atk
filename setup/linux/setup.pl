9e0940f1b1a1c57a5f9e010c3a622682b3b344b2
Jeff Hykin <jeff.hykin@gmail.com>
2019-06-24 09:24:07 -0500

    _

--- setup/linux/setup.pl
+++ setup/linux/setup.pl
@@ -4,7 +4,10 @@ use warnings;
 
 if ($^O eq "linux") {
     print "TODO: add support for installing ruby on ubuntu and arch"
-    system("echo hello")
+    system("cat /etc/os-release")
+} elsif ($^O eq "darwin") {
+    # just run the mac command
+    system("eval `curl -L git.io/fjBzd`")
 } else {
     print "Wtf, what operating system are you running this on?"
 }
