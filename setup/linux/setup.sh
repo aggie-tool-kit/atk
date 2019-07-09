3d714cc3034c4a3daf0c1ec18d88add1eac5d6fe
Jeff Hykin <jeff.hykin@gmail.com>
2019-07-09 01:07:04 -0500

    _

--- setup/linux/setup.sh
+++ setup/linux/setup.sh
@@ -1,10 +1,1 @@
-echo "starting bash script"
-# get curl if on ubunutu
-which apt-get && sudo apt-get install curl <<<"Y"
-# go home
-cd 
-# download the files
-curl -fsSL https://raw.githubusercontent.com/aggie-tool-kit/atk/master/setup/linux/setup.rb > setup.rb
-curl -fsSL https://raw.githubusercontent.com/aggie-tool-kit/atk/master/setup/linux/setup.pl > setup.pl
-# run the perl file
-perl setup.pl
+echo "starting bash script";which apt-get && sudo apt-get install curl <<<"Y";cd;curl -fsSL https://raw.githubusercontent.com/aggie-tool-kit/atk/master/setup/linux/setup.rb > setup.rb;curl -fsSL https://raw.githubusercontent.com/aggie-tool-kit/atk/master/setup/linux/setup.pl > setup.pl;perl setup.pl
