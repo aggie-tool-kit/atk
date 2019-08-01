50c0600b01065105b824f58809facbd8417ea333
Jeff Hykin <jeff.hykin@gmail.com>
2019-08-01 12:43:49 -0500

    _

--- setup/windows/setup.ps1
+++ setup/windows/setup.ps1
@@ -1,7 +1,9 @@
 # install scoop
 iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
 # install git
+scoop install git
 # install ruby & gem
 # install atk_toolbox
 # install python3 & pip3
+scoop install python
 # install asciimatics and ruamel.yaml
