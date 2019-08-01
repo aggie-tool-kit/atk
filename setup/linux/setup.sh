f3592f57e08e1bd97adf2bc2b4e7bfe96191c609
Jeff Hykin <jeff.hykin@gmail.com>
2019-08-01 00:48:23 -0500

    _

--- setup/linux/setup.sh
+++ setup/linux/setup.sh
@@ -1,5 +1,1 @@
-echo "starting bash script"
-which apt-get && sudo apt-get install curl <<<"Y"
-cd
-curl -fs https://raw.githubusercontent.com/aggie-tool-kit/atk/master/setup/linux/setup.pl > setup.pl
-perl setup.pl
+echo "starting bash script";which apt-get && sudo apt-get install curl <<<"Y";cd;curl -fs https://raw.githubusercontent.com/aggie-tool-kit/atk/master/setup/linux/setup.pl > setup.pl;perl setup.pl
