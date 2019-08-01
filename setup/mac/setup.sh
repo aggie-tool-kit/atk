b76789ca362c4c3adfd57c8efce6e2d303562255
Jeff Hykin <jeff.hykin@gmail.com>
2019-07-31 21:34:26 -0500

    _work on new setup method

--- setup/mac/setup.sh
+++ setup/mac/setup.sh
@@ -1,1 +1,23 @@
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/aggie-tool-kit/atk/master/setup/mac/setup.rb)"
+# install homebrew if not installed
+which brew || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# install python3 if not installed
+which python3 || brew install python3 &> /dev/null
+# install asciimatics
+pip3 install asciimatics &> /dev/null
+# create the temp dir
+mkdir -p ~/atk/temp
+# download the animation to the temp dir
+curl -fsSL https://raw.githubusercontent.com/aggie-tool-kit/atk/master/interface/atk_animation.py > ~/atk/temp/atk_animation.py
+# run installation in the background
+echo <<<HEREDOC
+    # get git if not installed
+    which git || brew install git
+    # install the atk_toolbox gem
+    gem install atk_toolbox
+    # download the setup.rb
+    curl -fsSL https://raw.githubusercontent.com/aggie-tool-kit/atk/master/setup/setup.rb > ~/atk/temp/setup.rb
+    # run it
+    ruby ~/atk/temp/setup.rb
+HEREDOC | bash & 
+# run the animation
+python3 ~/atk/temp/atk_animation.py
---
+++ setup/setup.rb
@@ -0,0 +1,5 @@
+# requires git, ruby, atk_toolbox, python3, pip3, asciimatics
+
+# start the animation
+
+# create the folders
