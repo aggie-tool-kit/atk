51594f3b38e69de2b2cbb9c2e5b20db2e49fb00a
Jeff Hykin <jeff.hykin@gmail.com>
2019-08-01 19:38:52 -0500

    _

--- setup/windows/setup.ps1
+++ setup/windows/setup.ps1
@@ -5,20 +5,20 @@ if (-not (cmd.exe /c "where scoop")) {
     iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
 }
 # go home
-cd %HOMEPATH%
+cd %USERPROFILE%
 
 # TODO:
     # - check if ruby is already installed, and what version
     # - check if python is already installed, and what version
-set PATH=%PATH%;%HOMEPATH%\scoop\shims
+set PATH=%PATH%;%USERPROFILE%\scoop\shims
 
 
 # install git
 scoop install git
 # install ruby & gem
 scoop install ruby
-set PATH=%PATH%;%HOMEPATH%\scoop\apps\current\bin\
-set PATH=%PATH%;%HOMEPATH%\scoop\apps\ruby\current\bin
+set PATH=%PATH%;%USERPROFILE%\scoop\apps\current\bin\
+set PATH=%PATH%;%USERPROFILE%\scoop\apps\ruby\current\bin
 # install atk_toolbox
 scoop\apps\current\bin\gem.cmd install atk_toolbox
 # install python3 & pip3
@@ -26,4 +26,4 @@ scoop install python
 # install asciimatics and ruamel.yaml
 scoop\apps\python\current\Scripts\pip.exe install asciimatics ruamel.yaml
 # download and run the script
-# ruby -e (new-object net.webclient).downloadstring('https://get.scoop.sh')
+# %USERPROFILE%\scoop\apps\ruby\current\bin\ruby.exe -e (new-object net.webclient).downloadstring('https://get.scoop.sh')
