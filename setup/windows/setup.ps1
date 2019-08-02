dd964c65a8d7be0188341e25f4c90c7420819085
Jeff Hykin <jeff.hykin@gmail.com>
2019-08-01 20:08:26 -0500

    _

--- setup/windows/setup.ps1
+++ setup/windows/setup.ps1
@@ -5,20 +5,20 @@ if (-not (cmd.exe /c "where scoop")) {
     iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
 }
 # go home
-cd C:\%HOMEPATH%
+cd C:\$HOMEPATH
 
 # TODO:
     # - check if ruby is already installed, and what version
     # - check if python is already installed, and what version
-set PATH=%PATH%;C:\%HOMEPATH%\scoop\shims
+set PATH=$PATH;C:\$HOMEPATH\scoop\shims
 
 
 # install git
 scoop install git
 # install ruby & gem
 scoop install ruby
-set PATH=%PATH%;C:\%HOMEPATH%\scoop\apps\current\bin
-set PATH=%PATH%;C:\%HOMEPATH%\scoop\apps\ruby\current\bin
+set PATH=$PATH;C:\$HOMEPATH\scoop\apps\current\bin
+set PATH=$PATH;C:\$HOMEPATH\scoop\apps\ruby\current\bin
 # install atk_toolbox
 scoop\apps\current\bin\gem.cmd install atk_toolbox
 # install python3 & pip3
@@ -26,4 +26,4 @@ scoop install python
 # install asciimatics and ruamel.yaml
 scoop\apps\python\current\Scripts\pip.exe install asciimatics ruamel.yaml
 # download and run the script
-C:\%HOMEPATH%\scoop\apps\ruby\current\bin\ruby.exe -e (new-object net.webclient).downloadstring('https://raw.githubusercontent.com/aggie-tool-kit/atk/master/setup/setup.rb')
+C:\$HOMEPATH\scoop\apps\ruby\current\bin\ruby.exe -e (new-object net.webclient).downloadstring('https://raw.githubusercontent.com/aggie-tool-kit/atk/master/setup/setup.rb')
