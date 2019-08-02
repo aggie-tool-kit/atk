bd54bfd062b4d35f1b6fb17c8a284b3a57f07cd0
Jeff Hykin <jeff.hykin@gmail.com>
2019-08-02 01:36:16 -0500

    _

--- setup/windows/setup.ps1
+++ setup/windows/setup.ps1
@@ -13,34 +13,64 @@ if (-not (cmd.exe /c "where scoop")) {
 cd $Home
 
 
-$program1 = @"
-Set oShell = WScript.CreateObject("WScript.Shell")
-filename = oShell.ExpandEnvironmentStrings("%TEMP%\resetvars.bat")
-Set objFileSystem = CreateObject("Scripting.fileSystemObject")
-Set oFile = objFileSystem.CreateTextFile(filename, TRUE)
-
-set oEnv=oShell.Environment("System")
-for each sitem in oEnv 
-    oFile.WriteLine("SET " & sitem)
-next
-path = oEnv("PATH")
-
-set oEnv=oShell.Environment("User")
-for each sitem in oEnv 
-    oFile.WriteLine("SET " & sitem)
-next
-
-path = path & ";" & oEnv("PATH")
-oFile.WriteLine("SET PATH=" & path)
-oFile.Close
-"@
-$program2 = @"
+$reset_command = @"
 @echo off
-%~dp0resetvars.vbs
-call "%TEMP%\resetvars.bat"
+::
+:: RefreshEnv.cmd
+::
+:: Batch file to read environment variables from registry and
+:: set session variables to these values.
+::
+:: With this batch file, there should be no need to reload command
+:: environment every time you want environment changes to propagate
+
+echo | set /p dummy="Reading environment variables from registry. Please wait... "
+
+goto main
+
+:: Set one environment variable from registry key
+:SetFromReg
+    "%WinDir%\System32\Reg" QUERY "%~1" /v "%~2" > "%TEMP%\_envset.tmp" 2>NUL
+    for /f "usebackq skip=2 tokens=2,*" %%A IN ("%TEMP%\_envset.tmp") do (
+        echo/set %~3=%%B
+    )
+    goto :EOF
+
+:: Get a list of environment variables from registry
+:GetRegEnv
+    "%WinDir%\System32\Reg" QUERY "%~1" > "%TEMP%\_envget.tmp"
+    for /f "usebackq skip=2" %%A IN ("%TEMP%\_envget.tmp") do (
+        if /I not "%%~A"=="Path" (
+            call :SetFromReg "%~1" "%%~A" "%%~A"
+        )
+    )
+    goto :EOF
+
+:main
+    echo/@echo off >"%TEMP%\_env.cmd"
+
+    :: Slowly generating final file
+    call :GetRegEnv "HKLM\System\CurrentControlSet\Control\Session Manager\Environment" >> "%TEMP%\_env.cmd"
+    call :GetRegEnv "HKCU\Environment">>"%TEMP%\_env.cmd" >> "%TEMP%\_env.cmd"
+
+    :: Special handling for PATH - mix both User and System
+    call :SetFromReg "HKLM\System\CurrentControlSet\Control\Session Manager\Environment" Path Path_HKLM >> "%TEMP%\_env.cmd"
+    call :SetFromReg "HKCU\Environment" Path Path_HKCU >> "%TEMP%\_env.cmd"
+
+    :: Caution: do not insert space-chars before >> redirection sign
+    echo/set Path=%%Path_HKLM%%;%%Path_HKCU%% >> "%TEMP%\_env.cmd"
+
+    :: Cleanup
+    del /f /q "%TEMP%\_envset.tmp" 2>nul
+    del /f /q "%TEMP%\_envget.tmp" 2>nul
+
+    :: Set these variables
+    call "%TEMP%\_env.cmd"
+
+    echo | set /p dummy="Done"
+    echo .
 "@
-New-Item -Path . -Name "resetvars.vb" -ItemType "file" -Value $program1
-New-Item -Path . -Name "resetvars.bat" -ItemType "file" -Value $program2
+New-Item -Path "$Home\AppData\local\Microsoft\WindowsApps\" -Name "RefreshEnv.cmd" -ItemType "file" -Value $reset_command
 
 
 $Env:path += "$Home\scoop\shims"
