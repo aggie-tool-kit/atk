574737265f60c5eae59a77c907c3c05c6e7f1cca
Jeff Hykin <jeff.hykin@gmail.com>
2019-08-01 23:41:44 -0500

    _fix missing dir creation

--- setup/setup.rb
+++ setup/setup.rb
@@ -5,6 +5,7 @@ require 'atk_toolbox'
 # create the file structure if it doesnt exist
 # 
 FileUtils.makedirs(HOME/"atk"/"installers")
+FileUtils.makedirs(HOME/"atk"/"temp")
 # download the files
 if not File.exist?(HOME/"atk"/"core.yaml")
     download('https://raw.githubusercontent.com/aggie-tool-kit/atk/master/interface/core.yaml'       , as: HOME/"atk"/"core.yaml")
