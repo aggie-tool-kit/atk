4aefa78db9581bd62092ff256bd98889cab4facd
Jeff Hykin <jeff.hykin@gmail.com>
2019-08-01 19:33:35 -0500

    _

--- setup/windows/setup.ps1
+++ setup/windows/setup.ps1
@@ -5,20 +5,24 @@ if (-not (cmd.exe /c "where scoop")) {
     iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
 }
 # go home
-cd
+cd %HOMEPATH%
 
 # TODO:
     # - check if ruby is already installed, and what version
     # - check if python is already installed, and what version
+set PATH=%PATH%;%HOMEPATH%\scoop\shims
+
 
 # install git
-scoop\shims\scoop.cmd install git
+scoop install git
 # install ruby & gem
-scoop\shims\scoop.cmd install ruby
+scoop install ruby
+set PATH=%PATH%;%HOMEPATH%\scoop\apps\current\bin\
+set PATH=%PATH%;%HOMEPATH%\scoop\apps\ruby\current\bin
 # install atk_toolbox
 scoop\apps\current\bin\gem.cmd install atk_toolbox
 # install python3 & pip3
-scoop\shims\scoop.cmd install python
+scoop install python
 # install asciimatics and ruamel.yaml
 scoop\apps\python\current\Scripts\pip.exe install asciimatics ruamel.yaml
 # download and run the script
