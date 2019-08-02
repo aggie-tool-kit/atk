8c69942c2b9368e746ca6e365b45d5b28eaaac63
Jeff Hykin <jeff.hykin@gmail.com>
2019-08-01 19:12:33 -0500

    _

--- setup/windows/setup.ps1
+++ setup/windows/setup.ps1
@@ -7,11 +7,19 @@ if (-not (cmd.exe /c "where scoop")) {
 # go home
 cd
 
+# TODO:
+    # - check if ruby is already installed, and what version
+    # - check if python is already installed, and what version
+
 # install git
 scoop\shims\scoop.cmd install git
 # install ruby & gem
+scoop\shims\scoop.cmd install ruby
 # install atk_toolbox
+scoop\apps\current\bin\gem.cmd install atk_toolbox
 # install python3 & pip3
 scoop\shims\scoop.cmd install python
 # install asciimatics and ruamel.yaml
+scoop\apps\python\current\Scripts\pip.exe install asciimatics ruamel.yaml
 # download and run the script
+# ruby -e (new-object net.webclient).downloadstring('https://get.scoop.sh')
